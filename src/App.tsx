ebdefbfb1a0852f47be6c01c6c3efc7138b195e2
Iury Nogueira <iunog@hotmail.com>
2021-10-29 10:27:25 -0300

    :sparkles: Adiciona componente para incremetar lista de natal #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,47 @@
-import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import NoelList, { ItemList } from "./components/NoelList/NoelList"
+import { CheckCircleIcon } from "@chakra-ui/icons"
+import React, { useEffect, useState } from 'react';
+import AddItem from "./components/AddItem/AddItem";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+
+  const [list, setList] = useState([
+    {
+      description: 'Papete do Senninha'
+    },
+    {
+      icon: <CheckCircleIcon />,
+      description: 'Power Ranger'
+    }
+  ]);
+
+  const [newItem, setNewItem] = useState<ItemList>({description: ''});
+  
+  useEffect(() => {
+    if (newItem && newItem.description !== '') {
+      setList([...list, newItem])
+    }
+  }, [newItem])
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <AddItem setNewItem={setNewItem} />
+            <NoelList data={list} />
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
